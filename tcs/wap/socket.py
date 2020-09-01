899d1b6dbd8c59d828a04b2cb34f2a84be462ed3
cSDes1gn <christian911@sympatico.ca>
2020-09-01 00:20:50 -0400

    adding registration/auth & unauth request calls

--- tcs/wap/socket.py
+++ tcs/wap/socket.py
@@ -23,6 +23,8 @@ from tcs.event.registry import EventRegistry
 
 class SocketHandler:
     
+    _socket_registry = [None for _ in range(4)]
+
     def __init__(self):
         self.log = logging.getLogger(__name__)
         self.soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -30,6 +32,7 @@ class SocketHandler:
             event.register('SHUTDOWN', self.shutdown)
             event.register('POST_REQUEST', self.post_request)
             event.register('GET_REQUEST', self.get_request)
+        self.conn, self.addr = None, None
         self.host = os.environ.get('HOSTNAME')
         self.port = int(os.environ.get('PORT'))
         # Uplink socket bind
@@ -52,17 +55,22 @@ class SocketHandler:
         while True:
             self.log.info("Waiting for receiver request on port %s", self.port)
             self.soc.listen()
-            #TODO: add dictionary of connected self.conn, self.addr tuples for concurrent ap connection
+            #TODO: fix this static implementation of self.conn and self.addr
             self.conn, self.addr = self.soc.accept()
             self.log.info("Connected by device at: %s", self.addr)
-            # receive a bitarray object
+            # make unauthenticated request to get apr key
             apr = self.get_request()
             self.log.info("Validating received APR key: %s", apr)
             # APR verification
             with EventRegistry() as event:
                 event.execute('VALIDATE_APR', apr)
+    
+    def register(self, ap_index:int):
+        """
+        """
+        self._socket_registry[ap_index] = (self.conn, self.addr)
 
-    def post_request(self, obj:object, msg:str=""):
+    def post_request(self, ap_index, obj:object, msg:str=""):
         """
         This function is bound to event:POST_REQUEST. It pickles an object to the client and sends
         an optional message.
@@ -70,20 +78,25 @@ class SocketHandler:
         :param obj: object to pickle to client
         :param msg: optional message to client
         """
-        with self.conn as conn:
+        with self._socket_registry[ap_index][0] as conn:
             conn.send(pickle.dumps(obj))
             if len(msg) > 0:
                 conn.send((msg).encode('utf-8'))
 
-    def get_request(self, size:int=2048):
+    def get_request(self, ap_index:int=-1, size:int=2048):
         """
         This function is bound to event:POST_REQUEST. It pickles an object to the client and sends
         an optional message.
 
+        :param ap_index: the access point socket for the get request. If None the request is unauthenticated
         :param size: size for byte load (default fo 2048)
         :return: object pickled from client
         """
-        with self.conn as conn:
+        if ap_index == -1:
+            connection = self.conn
+        else:
+            connection = self._socket_registry[ap_index][0]
+        with connection as conn:
             return pickle.loads(conn.recv(size))
 
     def shutdown(self):
