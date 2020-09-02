09856ae9d1dc40646bb83661bb014bcebcebb54c
cSDes1gn <christian911@sympatico.ca>
2020-09-02 00:37:23 -0400

    architecture fixes. and loosely mocking ER

--- tcs/tests/test_file_parser.py
+++ tcs/tests/test_file_parser.py
@@ -12,20 +12,26 @@ import os
 import shutil
 import unittest
 from pathlib import Path
+from unittest.mock import Mock
 
+from tcs.event.registry import EventRegistry
 from tcs.file.file_parser import FileParser
 
 
 class TestFileParser(unittest.TestCase):
 
     TEST_DIRS = ["samp", "/sampless", "m"]
-    CUBE_DIM = 4
+    os.environ['DIM'] = '4'
+    os.environ['PAYLOAD_DIR'] = "samples"
+
 
     # FIXME: hard to debug since if an error occurs during test cwd is never reset so tearDown
     # and subsequent setUp() functions fail causing the stack trace to be eclipsed.
     def setUp(self):
+        EventRegistry.execute = Mock()
         # create new directory for files
         os.mkdir("tcs/file/samples")
+        os.environ['PAYLOAD_DIR'] = "samples"
         self.parser = None
 
     def tearDown(self):
@@ -36,8 +42,8 @@ class TestFileParser(unittest.TestCase):
     def test_extract(self):
         """Sanity test"""
         test_file = open("tcs/file/samples/unittest.txt", "w")
-        self.parser = FileParser(self.CUBE_DIM, "samples")
-        self.parser.load()
+        self.parser = FileParser()
+        self.parser.load(1)
         test_file.close()
 
     def test_bad_dir(self):
@@ -45,26 +51,19 @@ class TestFileParser(unittest.TestCase):
         for test_dir in self.TEST_DIRS:
             with self.subTest(i=test_dir):
                 with self.assertRaises(FileNotFoundError):
-                    self.parser = FileParser(self.CUBE_DIM, test_dir)
+                    os.environ['PAYLOAD_DIR'] = test_dir
+                    self.parser = FileParser()
     
     def test_empty_dir(self):
         """Attempt to instantiate by passing an empty or non-existant directory."""
         with self.assertRaises(FileNotFoundError):
-            self.parser = FileParser(self.CUBE_DIM, "samples")
+            self.parser = FileParser()
     
     def test_bad_filetype(self):
         """Attempt to extract files of a bad type"""
         test_file = open("tcs/file/samples/unittest.yaml", "w")
         with self.assertRaises(TypeError):
-            self.parser = FileParser(self.CUBE_DIM, "samples")
-        test_file.close()
-
-    def test_bad_arguments(self):
-        """Attempt to pass in unexpected argument types"""
-        test_file = open("tcs/file/samples/unittest.yaml", "w")
-        with self.assertRaises(TypeError):
-            self.parser = FileParser([], "samples")
-            self.parser = FileParser(5, 3)
+            self.parser = FileParser()
         test_file.close()
 
 if __name__ == "__main__":
