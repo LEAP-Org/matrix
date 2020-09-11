388876679aec17f06183b68cdfe6e6a64241a27f
cSDes1gn <christian911@sympatico.ca>
2020-09-11 00:12:56 -0400

    finished interrupts and bit indexing

--- firmware/src/pilot.ino
+++ firmware/src/pilot.ino
@@ -17,30 +17,36 @@
 #define SER 14
 
 volatile int srclk = 0;
+volatile int ser_index = 0;
+
 const char *message = "Hello World!";
+bool tx_bits[strlen(message)*8];
 
 // Initial setup of the serial port, pins, and optional startup sequence
 void setup() {
     Serial.begin(9600);
     pinMode(RCLK, OUTPUT);
     pinMode(SRCLK, INPUT);
-    attachInterrupt(srclk_interrupt)
     pinMode(SER, OUTPUT);
+    attachInterrupt(digitalPinToInterrupt(SRCLK), srclk_interrupt, RISING);
 
     // represent each byte of message in its binary components
     for (int byte_index = 0; byte_index < strlen(message); byte_index++) {
         char tx_byte = message[byte_index];
         for (int bit_index = 0; bit_index < 8; bit_index++){
-            bool tx_bit = tx_byte & (0x80 >> bit_index);
+            tx_bits[(1+bit_index)+(8*byte_index)] = tx_byte & (0x80 >> bit_index);
         }
     }
 }
 
 void loop() {
-
+    
 }
 
 void srclk_interrupt() {
+    // Each shift clock pulse, set new bit on the serial bus and count up the pulses until the 8th 
+    // pulse is received
+    serial_out();
     if (srclk == 7){
         digitalWrite(RCLK, 1);
         srclk = 0;
@@ -50,6 +56,7 @@ void srclk_interrupt() {
 }
 
 void serial_out() {
-
+    digitalWrite(SER, tx_bits[ser_index]);
+    ser_index++;
 }
  
