942a43a4f05ec5d98758b8059306916a9564de4f
cSDes1gn <christian911@sympatico.ca>
2020-09-14 22:29:59 -0400

    sandboxing parsing testing

--- firmware/sandbox/parsing.ino
+++ firmware/sandbox/parsing.ino
@@ -9,19 +9,22 @@
  */
 
 #define SRCLK 7
+#define INT 12
 #define SER 4
+#define LEN 32
 
 volatile int ser_index = -1;
 
-const char *message = "Hello World!";
-bool tx_bits[12*8];
+const char *message = "LEAP";
+bool tx_bits[LEN];
 
 // Initial setup of the serial port, pins, and optional startup sequence
 void setup() {
     Serial.begin(9600);
     pinMode(SRCLK, INPUT);
     pinMode(SER, OUTPUT);
-    attachInterrupt(digitalPinToInterrupt(SRCLK), increment_ser_index, RISING);
+    pinMode(INT, OUTPUT);
+    // attachInterrupt(digitalPinToInterrupt(SRCLK), increment_ser_index, RISING);
 
     // represent each byte of message in its binary components
     for (int byte_index = 0; byte_index < strlen(message); byte_index++) {
@@ -33,17 +36,25 @@ void setup() {
 }
 
 void loop() {
-    
-}
-
-void increment_ser_index() {
-    // Each shift clock pulse, set new bit on the serial bus and count up the pulses until the 8th 
-    // pulse is received
+    // Phase 1 CLK +
+    digitalWrite(INT, HIGH);
     ser_index++;
-    digitalWrite(SER, tx_bits[ser_index]);
+    if (tx_bits[ser_index] == true) {
+        digitalWrite(SER, HIGH);
+    } else {
+        digitalWrite(SER, LOW);
+    }
+    delay(1000);
+    // Phase 2 CLK -
+    digitalWrite(INT, LOW);
+    // reset once max is reached
+    if (ser_index == LEN){
+        ser_index = -1;
+    }
+    delay(1000);
 }
 
-void serial_out() {
+// void increment_ser_index() {
     
-}
+// }
  
