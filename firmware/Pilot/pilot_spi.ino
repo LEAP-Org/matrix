692b3a9b701ecea3feff0383c0df4b543a5e6be3
cSDes1gn <christian911@sympatico.ca>
2020-10-21 00:25:21 -0400

    SPI complete and validated

--- firmware/Pilot/pilot_spi.ino
+++ firmware/Pilot/pilot_spi.ino
@@ -16,7 +16,7 @@
 void setup() {
     Serial.begin(9600);
     // Note SS is Digital pin 11 on UNO R3
-    // digitalWrite(SS, HIGH); // enforce SS stays high
+    digitalWrite(SS, HIGH); // enforce SS stays high
     SPI.begin();
     Serial.println("Setup Complete");
 }
@@ -24,16 +24,11 @@ void setup() {
 void loop() {
     byte c;
     SPI.beginTransaction(SPISettings(2000000, MSBFIRST, SPI_MODE0));
-    // for (const char * p = "LEAP"; c = *p; p++){
-    //     SPI.transfer(c);
-    // }
-    // digitalWrite(SS, LOW); // enforce SS stays high
-    SPI.transfer(0xFE);
+    for (const char * p = "LEAP"; c = *p; p++){
+        digitalWrite(SS, LOW);
+        SPI.transfer(c);
+        digitalWrite(SS, HIGH);
+        delay(10);
+    }
     SPI.endTransaction();
-    delay(1000);
-    // SPI.beginTransaction(SPISettings(2000000, MSBFIRST, SPI_MODE0));
-    // SPI.transfer(0xFF);
-    // // digitalWrite(SS, HIGH);
-    // SPI.endTransaction();
-    // delay(1000);
 }
