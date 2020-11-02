9e3d4184e6e1639315c23a9d1969729e52c70c17
cSDes1gn <christian911@sympatico.ca>
2020-11-02 00:57:15 -0500

    adding seven segment readout for frame mapping

--- firmware/src/main.ino
+++ firmware/src/main.ino
@@ -1,5 +1,5 @@
 /*
- * pilot_spi.ino
+ * main.ino
  * 
  * 8-Bit Development Tesseract Software Driver.
  * 
@@ -11,61 +11,83 @@
  * Copyright © 2020 LEAP. All Rights Reserved.
  */
 #include <SPI.h>
-#include "Segment.h"
+// #include "Segment.h"
 
 // Maximum amount of hex characters that can be stored to convert to binary
-int c = 0; // for incoming serial data
 
-Segment::Segment(int pin1, int pin2, int, pin3, int pin4, int pin5, int pin6, int pin7, int pin8) {
-    pins[0] = pin1;
-    pins[1] = pin2;
-    pins[2] = pin3;
-    pins[3] = pin4;
-    pins[4] = pin5;
-    pins[5] = pin6;
-    pins[6] = pin7;
-    pins[7] = pin8;
-    
-    for(int i = 0; i < 8; i++) {
-        pinMode(pins[i], OUTPUT);
-        digitalWrite(pins[i], HIGH);
-    }
-}
+class Segment {
+    private:
+        int pins[7];
 
-void Segment::displayHex(int number) {
+    public:
+        Segment(int pin1, int pin2, int pin3, int pin4, int pin5, int pin6, int pin7, boolean msb) {
+            // implementation for the 5621 BSR module
+            // pulled low to enable LED segment
+            if (msb == true){
+                this->pins[0] = pin1;
+                this->pins[1] = pin6;
+                this->pins[2] = pin2;
+                this->pins[3] = pin3;
+                this->pins[4] = pin4;
+                this->pins[5] = pin5;
+                this->pins[6] = pin7;
+            }else {
+                this->pins[0] = pin1;
+                this->pins[1] = pin2;
+                this->pins[2] = pin3;
+                this->pins[3] = pin4;
+                this->pins[4] = pin5;
+                this->pins[5] = pin6;
+                this->pins[6] = pin7;
+            }
+            
     
-    byte numbersToDisplay[] = {
-        B10001000,  //  0
-        B11101011,  //  1
-        B01001100,  //  2
-        B01001001,  //  3
-        B00101011,  //  4
-        B00011001,  //  5
-        B00011000,  //  6
-        B11001011,  //  7
-        B00001000,  //  8
-        B00001011,  //  9
-        B00001010,  //  A
-        B00111000,  //  B
-        B10011100,  //  C
-        B01101000,  //  D
-        B00011100,  //  E
-        B00011110,  //  F
-        B01011101  //  Error
-    };
-    
-    boolean bitToWrite;
-    
-    for(int segment = 0; segment < 8; segment++) {
-        if(number < 0 || number > 15) {
-            // display error
-            bitToWrite = bitRead(numbersToDisplay[16], segment);
-        }else {
-            bitToWrite = bitRead(numbersToDisplay[number], segment);
+            for(int i = 0; i < 7; i++) {
+                pinMode(pins[i], OUTPUT);
+                digitalWrite(pins[i], HIGH);
+            }
+        };
+
+        void displayHex(int hex) {
+            // mapped based on direct sequential pin mappings
+            byte bytestream[] = {
+                B0000010,  //  0
+                B1101110,  //  1
+                B1000001,  //  2
+                B1001000,  //  3
+                B0101100,  //  4
+                B0011000,  //  5
+                B0010000,  //  6
+                B1001110,  //  7
+                B0000000,  //  8
+                B0001000,  //  9
+                B0000100,  //  A
+                B0110000,  //  B
+                B0010011,  //  C
+                B1100000,  //  D
+                B0010001,  //  E
+                B0010101,  //  F
+                B1111101  //  Error
+            };
+            boolean bitToWrite;
+            
+            for(int segment = 0; segment < 7; segment++) {
+                if(hex < 0 || hex > 15) {
+                    // display error
+                    bitToWrite = bitRead(bytestream[16], segment);
+                }else {
+                    bitToWrite = bitRead(bytestream[hex], segment);
+                }
+                digitalWrite(pins[segment], bitToWrite);
+            }
         }
-        digitalWrite(pins[segment], bitToWrite);
-    }
-}
+};
+
+Segment segment_lsb(2, 3, 4, 5, 6, 7, 8, false);
+Segment segment_msb(9, 14, 15, 16, 17, 18, 19, true);
+byte msb;
+byte lsb;
+uint8_t c = 0; // for incoming serial data
 
 // Initial setup of the serial port, pins, and optional startup sequence
 void setup() {
@@ -73,21 +95,29 @@ void setup() {
     // Note SS is Digital pin 11 on UNO R3
     digitalWrite(SS, HIGH); // enforce SS stays high
     SPI.begin();
-    Segment segment_lsb(2, 3, 4, 5, 6, 7, 8);
-    Segment segment_msb(9, 14, 15, 16, 17, 18, 19);
 }
 
 void loop() {
-    // if (Serial.available() > 0) {
-    //     c = Serial.read();
-    // }
-    // SPI.beginTransaction(SPISettings(2000000, MSBFIRST, SPI_MODE0));
-    // digitalWrite(SS, LOW);
-    // SPI.transfer(c);
-    // digitalWrite(SS, HIGH);
-    // SPI.endTransaction();
-    c++;
-    Serial.println(c)
-    segment_lsb.displayHex(c)
-    segment_msb.displayHex(c)
+    if (Serial.available() > 0) {
+        c = Serial.read();
+        if (c==0xD || c==0xA){
+            Serial.println("Received D or A EOS byte");
+        } else {
+            msb = c >> 4;
+            lsb = c & 0x0F;
+            Serial.print("Received: ");
+            Serial.println(c, HEX);
+            Serial.println(msb, HEX);
+            Serial.println(lsb, HEX);
+            segment_lsb.displayHex(lsb);
+            segment_msb.displayHex(msb);
+            SPI.beginTransaction(SPISettings(2000000, MSBFIRST, SPI_MODE0));
+            digitalWrite(SS, LOW);
+            SPI.transfer(c);
+            digitalWrite(SS, HIGH);
+            SPI.endTransaction();
+        }
+        
+    }
+    delay(500);
 }
