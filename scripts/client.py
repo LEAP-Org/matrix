f0d2db5b4165df0876ef10d2449b6bf329ffa0b7
cSDes1gn <christian911@sympatico.ca>
2021-06-15 05:48:54 +0100

    update socket connection script for testing

--- scripts/client.py
+++ scripts/client.py
@@ -2,12 +2,11 @@
 
 import socket
 
-HOST = 'sedna.local'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
+HOST = 'localhost'  # The server's hostname or IP address
+PORT = 6000        # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
     data = s.recv(1024)
 
 print('Received', repr(data))
