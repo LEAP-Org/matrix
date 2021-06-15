c86447e85451ae93587bf2fd9543c1e25c6a33b4
cSDes1gn <christian911@sympatico.ca>
2021-06-15 04:27:49 +0100

    change registration endpoint to GET. add error handling and transmission enqueing

--- tcs/api/server.py
+++ tcs/api/server.py
@@ -1,29 +1,45 @@
-import asyncio
 from threading import Thread
-from flask import Flask, abort, request, jsonify
+from flask import Flask, request, abort, jsonify
+from flask.wrappers import Response
+from werkzeug.exceptions import BadRequest, HTTPException, Unauthorized
 from tcs.cache.cache import FrameCache
 from tcs.tcp.socket import SocketInterface
-
+from tcs.event.registry import Registry as events
 
 app = Flask(__name__)
 
 
-@app.route('/v1/register', methods=['POST'])
-def register() -> str:
-    payload: dict = request.json
-    apr = payload.get('apr')
-    if apr is None:
+@app.route('/v1/register', methods=['GET'])
+def register() -> Response:
+    apr_key = request.headers['Apr']
+    if apr_key is None:
         abort(400)
     with FrameCache() as fc:
-        result = fc.get(apr)
+        result = fc.get(apr_key)
     if not result:
         abort(401)
     # TODO: run random port selection on set of available ports
     port = 6000
     # start new socket connection
     socket = SocketInterface(addr="localhost:{}".format(port))
-    Thread(name=apr, target=socket.run, args=(), daemon=True).start()
+    Thread(name=apr_key, target=socket.run, args=(), daemon=True).start()
+    # enqueue new data for sending
+    events.enqueue.execute("Hello World!".encode('utf-8'))
     payload = {
         'port': port
     }
     return jsonify(payload)
+
+
+@app.errorhandler(400)
+def bad_request(_):
+    response = jsonify({'message': 'bad request'})
+    response.status_code = 400
+    return response
+
+
+@app.errorhandler(401)
+def unauthorized(_):
+    response = jsonify({'message': 'unauthorized'})
+    response.status_code = 401
+    return response
